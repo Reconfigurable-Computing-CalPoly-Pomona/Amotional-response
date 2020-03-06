56d02a51fb0d3c4fdb96565f94a78a4a5af9f6d0
Gavin Worsley <kgeekmail@gmail.com>
2020-03-06 15:05:34 -0800

    Confirmed faces work, updated outputs

--- pi-inference.py
+++ pi-inference.py
@@ -6,16 +6,19 @@ import tensorflow as tf
 import numpy as np
 end = time.time()
 
-print("Imports completed in ", (end-start))
+print("[INFO] Imports completed in ", (end-start), '\n')
 
 dimensions = (1,48,48,1)
+
+NUM_CYCLES = 10
+
 cascade_data_loc = '/home/kevin-worsley/opencv/data/haarcascades/haarcascade_frontalface_default.xml'
-#cascade_data_loc = '/usr/local/lib/python3.7/dist-packages/cv2/data/haarcascade_frontalface_default.xml'
+cascade_data_loc = '/usr/local/lib/python3.7/dist-packages/cv2/data/haarcascade_frontalface_default.xml'
 
 face_cascade = cv2.CascadeClassifier()
 face_cascade.load(cascade_data_loc)
 
-print("Haar Cascade detector loaded")
+print("[SUCCESS] Haar Cascade detector loaded \n")
 
 interpreter = tf.lite.Interpreter(model_path='cnn_emotion.tflite')
 interpreter.allocate_tensors()
@@ -23,15 +26,15 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-print("TFLite Interpreter initialized")
+print("[SUCCESS] TFLite Interpreter initialized \n")
 
 webcam = cv2.VideoCapture(0)
 
-for i in range(10):
+for i in range(NUM_CYCLES):
     success, frame = webcam.read()
 
     if(success):
-        print("---------Frame captured-----------")
+        print("[SUCCESS] Frame captured")
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -42,20 +45,21 @@ for i in range(10):
             faceROI = frame[(y):y + (h), x:(x) + (w)]  # -2 gets rid of the bounding box
 
         if (len(faces) != 0):
-            print("Found a face")
-
-            image_resized = cv2.resize(faceROI, (48, 48), interpolation=cv2.INTER_AREA)
+            print("   [SUCCESS] Found a face \n")
+            image_resized = cv2.resize(faceROI, (48, 48), cv2.INTER_AREA)
+            print("      [INFO] Image resize to ROI succeeded")
 
             image_to_model = np.reshape(image_resized, (1, 48, 48, 1))
+            print("      [INFO] Image is reshaped for model")
 
             image_to_model = image_to_model.astype('float32') / 255.0  # tf.keras.utils.normalize(image_to_model, axis=0)
+            print("      [INFO] Image is normalized")
 
             print(image_to_model.shape)
-            print("Image ready to pass to model \n")
+            print("      [INFO] Image ready to pass to model \n")
 
         else:
-            print("Could not find a face \n")
-
+            print("   [FAIL] Could not find a face \n")
 
 
 
@@ -63,8 +67,8 @@ for i in range(10):
 
 
     else:
-        print("Frame did not open")
+        print("[FAIL] Frame did not open")
 
+print('[INFO] Exiting...')
 webcam.release()
 cv2.destroyAllWindows()
-
