54bfefe53d4f1ac2cd6bbc254f05c406168b8b5d
Gavin Worsley <kgeekmail@gmail.com>
2020-03-07 22:20:25 -0800

    Integrated Tensorflow Lite, camera looping
    Detections are accurate with the full model, and camera takes single image per iteration

--- pi-inference.py
+++ pi-inference.py
@@ -12,8 +12,9 @@ dimensions = (1,48,48,1)
 
 NUM_CYCLES = 10
 
-cascade_data_loc = '/home/kevin-worsley/opencv/data/haarcascades/haarcascade_frontalface_default.xml'
+#cascade_data_loc = '/home/kevin-worsley/opencv/data/haarcascades/haarcascade_frontalface_default.xml'
 cascade_data_loc = '/usr/local/lib/python3.7/dist-packages/cv2/data/haarcascade_frontalface_default.xml'
+save_dir = '/home/pi/photos_to_test/'
 
 face_cascade = cv2.CascadeClassifier()
 face_cascade.load(cascade_data_loc)
@@ -25,16 +26,22 @@ interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+#model_load = tf.keras.models.load_model('cnn_face_tolite.model')
 
 print("[SUCCESS] TFLite Interpreter initialized \n")
 
-webcam = cv2.VideoCapture(0)
-
-for i in range(NUM_CYCLES):
+for i in range(10):
+    webcam = cv2.VideoCapture(0)
+    print("Smile!")
+    time.sleep(1)
+    start_photos = time.time()
     success, frame = webcam.read()
 
     if(success):
-        print("[SUCCESS] Frame captured")
+        print("[SUCCESS] Frame " + str(i) + " captured at ", (time.time()- start_photos))
+        webcam.release() #close the cam, we already took the photo
+        filename = save_dir + 'testimage' + str(i) + '.jpg'
+        cv2.imwrite(filename, frame)
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -42,7 +49,7 @@ for i in range(NUM_CYCLES):
 
         for (x, y, w, h) in faces:
             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            faceROI = frame[(y):y + (h), x:(x) + (w)]  # -2 gets rid of the bounding box
+            faceROI = frame[(y):y + (h), x:(x) + (w)]
 
         if (len(faces) != 0):
             print("   [SUCCESS] Found a face \n")
@@ -58,6 +65,27 @@ for i in range(NUM_CYCLES):
             print(image_to_model.shape)
             print("      [INFO] Image ready to pass to model \n")
 
+            start_detect = time.time()
+            interpreter.set_tensor(input_details[0]['index'], image_to_model)
+            interpreter.invoke()
+
+            result = interpreter.get_tensor(output_details[0]['index'])
+            print(result)
+            emotion = np.argmax(result)
+            end_detect = time.time()
+
+            if(emotion ==0):
+                print("     [RESULT] Face is angry \n")
+            elif(emotion == 1):
+                print("     [RESULT] Face is happy \n")
+            elif(emotion == 2):
+                print("     [RESULT] Face is sad \n")
+            elif(emotion == 3):
+                print("     [RESULT] Face is neutral \n")
+            else:
+                print("[ERROR] Unreachable point in code, should not trigger")
+
+            print("[INFO] Operation took ", (end_detect-start_detect), "\n\n\n\n\n\n")
         else:
             print("   [FAIL] Could not find a face \n")
 
@@ -70,5 +98,4 @@ for i in range(NUM_CYCLES):
         print("[FAIL] Frame did not open")
 
 print('[INFO] Exiting...')
-webcam.release()
 cv2.destroyAllWindows()
